f81280c9283a612cb300b8956d7b057c1e0b7037
Maxson <77611206+Maxson71@users.noreply.github.com>
2024-08-29 06:26:39 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import TodoList from "@/components/TodoList";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <h1 className="p-4">Todo List</h1>
